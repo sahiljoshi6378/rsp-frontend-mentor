baf681c3c66c271b9f763037e5408c87731441cf
Sahil  Joshi <sahiljoshi6378@gmail.com>
2022-03-15 17:05:00 +0530

    initial layout styled

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,44 @@
 const rulesBtn = document.getElementById("rules_toggle");
 
-function fadeIn() {
-  const popover = document.getElementsByClassName("rules__popover")[0];
-}
 
+
+// open rules banner
 rulesBtn.addEventListener("click", e => {
+
+  // const closeBtn = document.createElement('img');
+  // closeBtn.
+
   const POPOVER__HTML = `
   <div class="rules__popover ">
-  <div class="popover__inner flex flex__fdc flex__aic flex__jcsb">
+  <div class="popover__inner flex flex__fdc flex__aic flex__jcfs">
     <p>Rules</p>
     <img src="./asstes/svg/image-rules.svg" alt="rules of game">
+    <img
+    onClick="closeRuleBanner()"
+    class="close"
+    src="./asstes/svg/close.svg"
+    />
   </div>
 </div>
     `;
 
-  const offsetInnerWrapper = document.getElementsByClassName(
-    "offset__inner__wrapper"
-  )[0];
+  const gameWrapper = document.getElementsByClassName("game__wrapper")[0];
 
   if (e.target.dataset.state === "false") {
     e.target.dataset.state = "true";
-    offsetInnerWrapper.insertAdjacentHTML("beforeend", POPOVER__HTML);
+    closeRulesbtn = document.getElementsByClassName("close")[0];
+    gameWrapper.insertAdjacentHTML("afterbegin", POPOVER__HTML);
   } else if (e.target.dataset.state === "true") {
     e.target.dataset.state = "false";
     const popover = document.getElementsByClassName("rules__popover")[0];
-
     popover.remove();
   }
 });
+
+
+
+function closeRuleBanner(e){
+  rulesBtn.dataset.state = "false";
+  const popover = document.getElementsByClassName("rules__popover")[0];
+  popover.remove();
+}
